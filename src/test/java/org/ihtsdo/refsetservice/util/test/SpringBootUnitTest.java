5538a059a0a55d35ed5c9e95750e846bec75dc2e
jefron <jefron@westcoastinformatics.com>
2021-04-08 11:29:59 -0700

    Add basic SpringBoot test that makes rest call against google.com

---
+++ src/test/java/org/ihtsdo/refsetservice/util/test/SpringBootUnitTest.java
@@ -0,0 +1,40 @@
+package org.ihtsdo.refsetservice.util.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Validates the code-system-* files in the "src/resources" folder.
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
+public class SpringBootUnitTest {
+
+    /** The logger. */
+    private final Logger logger = LoggerFactory.getLogger(SpringBootUnitTest.class);
+
+    @Test
+    public void testConnectability() throws Exception {
+        logger.info("TEST");
+        final Client client = ClientBuilder.newClient();
+        final WebTarget target = client.target("https://www.google.com");
+
+        final Response response = target.request("application/json").get();
+
+        assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
+    }
+}
