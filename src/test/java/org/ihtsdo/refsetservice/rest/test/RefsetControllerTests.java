c35f1b038ececc3d18d7fc7bc5ed665ebb72ae0a
twkatadin <twkatadin@gmail.com>
2021-04-26 18:59:23 -1000

    RT2-27: Fixed language selection for refset members

--- src/test/java/org/ihtsdo/refsetservice/rest/test/RefsetControllerTests.java
+++ src/test/java/org/ihtsdo/refsetservice/rest/test/RefsetControllerTests.java
@@ -7,10 +7,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.List;
 
+import org.apache.lucene.queryparser.classic.QueryParserBase;
+import org.ihtsdo.refsetservice.model.PfsParameter;
 import org.ihtsdo.refsetservice.model.Refset;
 import org.ihtsdo.refsetservice.service.TerminologyService;
 import org.ihtsdo.refsetservice.test.BaseTest;
 import org.ihtsdo.refsetservice.util.ConceptResultList;
+import org.ihtsdo.refsetservice.util.ResultList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -32,7 +35,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class RefsetControllerTests extends BaseTest {
 
     /** The Constant TESTING_REFSET_ID. */
-    private static final String TESTING_REFSET_ID = "551000172106";
+    private static final String TESTING_REFSET_ID = "551000172106"; // this code works for sure: "551000172106";
 
     /** The logger. */
     private static Logger logger = LoggerFactory.getLogger(RefsetControllerTests.class);
@@ -74,15 +77,30 @@ public class RefsetControllerTests extends BaseTest {
         MvcResult result = null;
         String content = null;
         Refset refset = null;
+        
+        String refsetTerminologyId = null;
 
-        url = baseUrl + "/d11dfa11-d2c2-4f6d-a85a-32071fd49c42";
+        try (final TerminologyService service = new TerminologyService()) {
+            
+            ResultList<Refset> refsets= service.find(
+                    "refsetId:" + QueryParserBase.escape(TESTING_REFSET_ID) + "", new PfsParameter(0, 1), Refset.class, null);
+            
+            assertThat(refsets.getItems().size()).isGreaterThan(0);
+            
+            refset = refsets.getItems().get(0);
+            assertThat(refset).isNotNull();
+            
+            refsetTerminologyId = refset.getId();
+        }
+
+        url = baseUrl + "/" + refsetTerminologyId;
         logger.info("Testing url - " + url);
         result = mvc.perform(get(url)).andExpect(status().isOk()).andReturn();
         content = result.getResponse().getContentAsString();
         logger.info(" content = " + content);
         refset = new ObjectMapper().readValue(content, Refset.class);
         assertThat(refset).isNotNull();
-        assertThat(refset.getRefsetId()).isEqualTo("001");
+        assertThat(refset.getRefsetId()).isEqualTo(TESTING_REFSET_ID);
 
     }
 
@@ -99,21 +117,19 @@ public class RefsetControllerTests extends BaseTest {
         String content = null;
         ConceptResultList members = null;
         String refsetTerminologyId = null;
-
+        
         try (final TerminologyService service = new TerminologyService()) {
-            List<Refset> allRefsets = service.getAll(Refset.class);
-
-            for (Refset refset : allRefsets) {
-                if (refset.getRefsetId().equals(TESTING_REFSET_ID)) {
-                    refsetTerminologyId = refset.getId();
-                    break;
-                }
-            }
-        }
-
-        if (refsetTerminologyId == null) {
-            throw new Exception(
-                    "Unable to find refset id " + refsetTerminologyId + " in the database");
+            
+            ResultList<Refset> refsets= service.find(
+                    "refsetId:" + QueryParserBase.escape(TESTING_REFSET_ID) + "", new PfsParameter(0, 1), Refset.class, null);
+            
+            assertThat(refsets.getItems().size()).isGreaterThan(0);
+            
+            Refset refset = refsets.getItems().get(0);
+            assertThat(refset).isNotNull();
+            assertThat(refset.getRefsetId()).isEqualTo(TESTING_REFSET_ID);
+            
+            refsetTerminologyId = refset.getId();
         }
 
         url = baseUrl + "/" + refsetTerminologyId + "/members?limit=10&offset=0"; // 5a2f0f94-da88-4b20-a6b5-ca9990fbbc1f
@@ -124,10 +140,7 @@ public class RefsetControllerTests extends BaseTest {
         members = new ObjectMapper().readValue(content, (ConceptResultList.class));
         assertThat(members).isNotNull();
         assertThat(members.getItems().size()).isGreaterThan(0);
-        // assertThat(members.getItems().get(0).getCode()).isEqualTo("162290004");
         assertThat(members.getItems().get(0).getDescriptions().size()).isGreaterThan(0);
-        // assertThat(members.getItems().get(0).getDescriptions().get(0).get("term")).isEqualTo("Dry
-        // eyes");
 
         logger.info("Done -- Just returned refset with " + members.size() + " members.");
     }
