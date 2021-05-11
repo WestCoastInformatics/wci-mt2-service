6cffcb179c9dec093a5b42767661da0ffffdd40e
twkatadin <twkatadin@gmail.com>
2021-05-10 23:58:37 -1000

    RT2-84: Added methods to generate member sctid list export

--- src/main/java/org/ihtsdo/refsetservice/terminologyservice/RefsetMemberService.java
+++ src/main/java/org/ihtsdo/refsetservice/terminologyservice/RefsetMemberService.java
@@ -3,11 +3,15 @@
  */
 package org.ihtsdo.refsetservice.terminologyservice;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -30,6 +34,8 @@ import org.ihtsdo.refsetservice.model.Edition;
 import org.ihtsdo.refsetservice.model.Refset;
 import org.ihtsdo.refsetservice.service.TerminologyService;
 import org.ihtsdo.refsetservice.util.ConceptResultList;
+import org.ihtsdo.refsetservice.util.DateUtility;
+import org.ihtsdo.refsetservice.util.PropertyUtility;
 import org.ihtsdo.refsetservice.util.SearchParameters;
 import org.ihtsdo.refsetservice.util.TaxonomyParameters;
 import org.slf4j.Logger;
@@ -79,8 +85,15 @@ public class RefsetMemberService {
 
     /** The other description type. */
     private static final String TYPE_OTHER_PT = "OTHER";
+    
+    /** The local directory to store exported refset files. */
+    private static String EXPORT_FILE_DIR;
+
 
     static {
+        
+        EXPORT_FILE_DIR = PropertyUtility.getProperty("export.fileDir");
+        
         // TODO: Remove once Edition updated
         refsetToLanguagesMap.put("450828004", "es");
         refsetToLanguagesMap.put("32570271000036106", "en");
@@ -542,27 +555,140 @@ public class RefsetMemberService {
      * @return the refset member concepts
      * @throws Exception the exception
      */
-    public static String exportRefsetRf2(String refsetId, String type, String fileNameDate, 
-    		String startEffectiveTime, String transientEffectiveTime, String branchPath) throws Exception {
+    public static String exportRefsetRf2(final String refsetId, final String type, final String fileNameDate, 
+        final String startEffectiveTime, final String transientEffectiveTime, final String branchPath) throws Exception {
+        
+        String url = SnowstormConnection.BASE_URL + "exports";
+        
+        String entity = "{\"refsetIds\": [\"" + refsetId + "\"],  \"branchPath\": \"" + 
+                branchPath + "\", \"conceptsAndRelationshipsOnly\": \"false\", \"filenameEffectiveDate\": \"" + 
+                fileNameDate + "\", \"legacyZipNaming\": \"false\", \"type\": \"" + 
+                type + "\", \"unpromotedChangesOnly\": \"false\"" +  
+                (startEffectiveTime == null ? "" : ",  \"startEffectiveTime\": \"" + startEffectiveTime + "\"") +
+                (transientEffectiveTime == null ? "" : ",  \"transientEffectiveTime\": \"" + transientEffectiveTime + "\"") +"}";
 
-        try (final TerminologyService service = new TerminologyService()) {
-
-            
-            String url = SnowstormConnection.BASE_URL + "exports";
-            
-            String entity = "{\"refsetIds\": [\"" + refsetId + "\"],  \"branchPath\": \"" + 
-            		branchPath + "\", \"conceptsAndRelationshipsOnly\": \"false\", \"filenameEffectiveDate\": \"" + 
-            		fileNameDate + "\", \"legacyZipNaming\": \"false\", \"type\": \"" + 
-            		type + "\", \"unpromotedChangesOnly\": \"false\"" +  
-            		(startEffectiveTime == null ? "" : ",  \"startEffectiveTime\": \"" + startEffectiveTime + "\"") +
-            		(transientEffectiveTime == null ? "" : ",  \"transientEffectiveTime\": \"" + transientEffectiveTime + "\"") +"}";
-            
-            logger.info("Snowstorm URL: " + url + entity);
-            final Response response = SnowstormConnection.postResponse(url, entity);
+        logger.info("Snowstorm URL: " + url + entity);
+        
+        try (Response response = SnowstormConnection.postResponse(url, entity)) {
 
             logger.info("Response location " + response.getLocation());
-            
             return response.getLocation().toString();
+            
+        } catch (Exception ex) {
+
+            logger.error("Could not export refset" + ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+    
+    /**
+     * Get the refset member basic information.
+     *
+     * @param refsetId the refset ID
+     * @param offset the 0 based page number to get
+     * @param limit the number of results per page
+     * @param branchPath the branch and version of the refset
+     * @return the raw resultString
+     * @throws Exception the exception
+     */
+    private static String getMemberSctids(final String refsetId, final int offset, 
+        final int limit, final String branchPath) throws Exception {
+        
+        final String pagingParams =
+                "offset=" + (offset * limit)
+                        + "&limit=" + limit;
+        
+        String url = SnowstormConnection.BASE_URL + "browser/" + branchPath + "/members?referenceSet=" + refsetId + "&" + pagingParams;
+
+        logger.info("Snowstorm URL: " + url);
+        
+        try (Response response = SnowstormConnection.getResponse(url)) {
+
+            final String resultString = response.readEntity(String.class);
+            return resultString;
+            
+        } catch (Exception ex) {
+
+            logger.error("Could not retrieve refset members" + ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+    
+    /**
+     * Export the refset member concept IDs in a zipped CSV format.
+     *
+     * @param refsetId the refset ID
+     * @param branchPath the branch path
+     * @return the URL of the file containing the member list
+     * @throws Exception the exception
+     */
+    public static String exportRefsetSctidList(final String refsetId) throws Exception {
+        
+        int offset = 0;
+        int limit = 10000;
+        String branchPath = "";
+        boolean morePages = true;
+        StringBuilder fileLines = new StringBuilder();
+        String outputPath = EXPORT_FILE_DIR + "/";
+        String versionDate = "";
+        String pathDate = "";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        
+        logger.info("    output path = " + outputPath);
+        
+        try (
+            final TerminologyService service = new TerminologyService();
+            final FileOutputStream fileOutputStream = new FileOutputStream(outputPath);
+            final OutputStreamWriter outputStreamWriter =
+                new OutputStreamWriter(fileOutputStream, "UTF-8");
+            final PrintWriter printWriter = new PrintWriter(outputStreamWriter);
+        ) {
+
+            final Refset refset = service.get(refsetId, Refset.class);
+            
+            
+    
+            if (refset.getVersionDate() != null) {
+    
+                versionDate = simpleDateFormat.format(refset.getVersionDate());
+                // pathDate = "/" + versionDate;
+            } else {
+                versionDate = simpleDateFormat.format(new Date());
+            }
+            
+            outputPath += "refset_" + refset.getRefsetId() + "_" + versionDate + "_member_ids.txt";
+            branchPath = refset.getEdition().getBranch() + pathDate;
+            
+            while (morePages) {
+                
+                final String resultString = getMemberSctids(refset.getRefsetId(), offset, limit, branchPath);
+
+                final ObjectMapper mapper = new ObjectMapper();
+                final JsonNode root = mapper.readTree(resultString.toString());
+                int totalPages = (root.get("totalPages")).asInt();
+                offset = (root.get("number")).asInt();
+                final JsonNode items = root.get("items");
+                final Iterator<JsonNode> iterator = items.iterator();
+                
+                if (offset == totalPages - 1) {
+                    morePages = false;
+                }
+
+                while (iterator.hasNext()) {
+
+                    final JsonNode item = iterator.next();
+                    final String conceptId = (item.get("referencedComponentId").asText());
+                    fileLines.append(conceptId + "\n");
+                }
+            }
+            
+            printWriter.print(fileLines);
+            
+            return outputPath;
             
         } catch (Exception ex) {
 
