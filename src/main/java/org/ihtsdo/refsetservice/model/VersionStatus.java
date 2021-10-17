8f28969c9d5c950f69ed7c5a81d4ceed6d0cfbd3
Nuno Marques <nuno.marques@gmail.com>
2021-10-16 20:10:50 -0700

    RT2-343: Fix version status filter - correct previous commit

--- src/main/java/org/ihtsdo/refsetservice/model/VersionStatus.java
+++ src/main/java/org/ihtsdo/refsetservice/model/VersionStatus.java
@@ -3,12 +3,10 @@ package org.ihtsdo.refsetservice.model;
 public enum VersionStatus {
 
     /** Captures all non-published status. */
-    IN_DEVELOPMENT("In Development"),
-    //IN_DEVELOPMENT("IN DEVELOPMENT"),
+    IN_DEVELOPMENT("IN DEVELOPMENT"),
 
     /** The published status. */
-    PUBLISHED("Published");
-    //PUBLISHED("PUBLISHED");
+    PUBLISHED("PUBLISHED");
 
     private final String label;
 
