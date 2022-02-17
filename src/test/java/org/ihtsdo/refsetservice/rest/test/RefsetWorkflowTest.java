6b05805200722edda494d8ec386ed3f4a2264eb9
Jesse Efron <jesseefron@Jesses-MacBook-Pro.local>
2022-02-17 15:55:52 -0800

    renaming for easier identification

--- src/test/java/org/ihtsdo/refsetservice/rest/test/RefsetWorkflowTest.java
+++ src/test/java/org/ihtsdo/refsetservice/rest/test/RefsetWorkflowTest.java
@@ -79,11 +79,11 @@ public class RefsetWorkflowTest extends AbstractRefsetTests {
 
             if (firstTimeSetup) {
 
-                testingProjectId = getUtil.getProjectInternalId(TESTING_PROJECT_NAME);
-                testingEditionId = getUtil.getEditionInternalId(TESTING_EDITION_NAME);
+                testingProjectId = getUtil.getInternalProjectId(TESTING_PROJECT_NAME);
+                testingEditionId = getUtil.getInternalEditionId(TESTING_EDITION_NAME);
 
-                mainNrcTestingRefsetInternalId = getUtil.getRefsetInternalId(MAIN_NRC_TESTING_REFSET_ID, MAIN_NRC_TESTING_REFSET_VERSION);
-                mainCoreTestingRefsetInternalId = getUtil.getRefsetInternalId(MAIN_CORE_TESTING_REFSET_ID, MAIN_CORE_TESTING_REFSET_VERSION);
+                mainNrcTestingRefsetInternalId = getUtil.getInternalRefsetId(MAIN_NRC_TESTING_REFSET_ID, MAIN_NRC_TESTING_REFSET_VERSION);
+                mainCoreTestingRefsetInternalId = getUtil.getInternalRefsetId(MAIN_CORE_TESTING_REFSET_ID, MAIN_CORE_TESTING_REFSET_VERSION);
 
                 editUtil = new EditUnitTestUtilities(mvc, baseUrl, SIMPLE_DATE_FORMAT, testingProjectId, testingEditionId);
 
@@ -364,7 +364,7 @@ public class RefsetWorkflowTest extends AbstractRefsetTests {
         // verify the new version
         try (final TerminologyService service = new TerminologyService()) {
 
-            refsetInternalId = getUtil.getRefsetInternalId(refsetId, versionDate);
+            refsetInternalId = getUtil.getInternalRefsetId(refsetId, versionDate);
             refset = service.get(refsetInternalId, Refset.class);
             assertThat(refset).isNotNull();
             releaseBranchPath = refset.getEdition() + "/" + refset.getVersionDate();
