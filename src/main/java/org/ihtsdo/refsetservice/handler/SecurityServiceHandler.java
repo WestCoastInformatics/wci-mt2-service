482ae50d8fd8924298fefce563d252d314ac4c41
Nuno Marques <nuno.marques@gmail.com>
2023-03-20 11:31:57 -0700

    add ims url

--- src/main/java/org/ihtsdo/refsetservice/handler/SecurityServiceHandler.java
+++ src/main/java/org/ihtsdo/refsetservice/handler/SecurityServiceHandler.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2023 SNOMED International - All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of SNOMED International
+ * The intellectual and technical concepts contained herein are proprietary to
+ * SNOMED International and may be covered by U.S. and Foreign Patents, patents in process,
+ * and are protected by trade secret or copyright law.  Dissemination of this information
+ * or reproduction of this material is strictly forbidden.
+ */
 package org.ihtsdo.refsetservice.handler;
 
 import org.ihtsdo.refsetservice.model.Configurable;
@@ -8,29 +17,44 @@ import org.ihtsdo.refsetservice.model.User;
  */
 public interface SecurityServiceHandler extends Configurable {
 
-	/**
-	 * Authenticate.
-	 *
-	 * @param user     the user
-	 * @return the user
-	 * @throws Exception
-	 */
-	public User authenticate(String user) throws Exception;
+    /**
+     * Authenticate.
+     *
+     * @param user the user
+     * @return the user
+     * @throws Exception
+     */
+    public User authenticate(String user) throws Exception;
 
-	/**
-	 * Indicates whether or not the user should be timed out.
-	 *
-	 * @param user the user
-	 * @return true, if successful
-	 */
-	public boolean timeoutUser(String user);
+    /**
+     * Returns the authenticate url.
+     *
+     * @return the authenticate url
+     * @throws Exception the exception
+     */
+    public String getAuthenticateUrl() throws Exception;
+    
+    /**
+     * Returns the logout url.
+     *
+     * @return the logout url
+     * @throws Exception the exception
+     */
+    public String getLogoutUrl() throws Exception;
 
-	/**
-	 * Computes token for user. For example, a UUID or an MD5 or a counter. Each
-	 * login requires yields a potentially different token, even for the same user.
-	 *
-	 * @param user the user
-	 * @return the string
-	 */
-	public String computeTokenForUser(String user);
+    /**
+     * Indicates whether or not the user should be timed out.
+     *
+     * @param user the user
+     * @return true, if successful
+     */
+    public boolean timeoutUser(String user);
+
+    /**
+     * Computes token for user. For example, a UUID or an MD5 or a counter. Each login requires yields a potentially different token, even for the same user.
+     *
+     * @param user the user
+     * @return the string
+     */
+    public String computeTokenForUser(String user);
 }
