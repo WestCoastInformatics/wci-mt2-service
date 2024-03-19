9f187fcfd51f79aaa91f020f6369906f4f949d33
Nuno Marques <nuno.marques@gmail.com>
2024-03-19 16:14:21 -0700

    WMT-100: enable database

--- src/main/java/org/ihtsdo/refsetservice/model/MapNote.java
+++ src/main/java/org/ihtsdo/refsetservice/model/MapNote.java
@@ -3,24 +3,28 @@ package org.ihtsdo.refsetservice.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlID;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * The Map Note Jpa object
  */
-// @Entity
-// @Table(name = "map_notes")
+@Entity
+@Table(name = "map_notes")
 @JsonIgnoreProperties(ignoreUnknown = true)
-// @Indexed
+@Indexed
 public class MapNote extends AbstractHasId {
 
     /** The id. */
@@ -145,23 +149,30 @@ public class MapNote extends AbstractHasId {
     @Override
     public boolean equals(final Object obj) {
 
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         final MapNote other = (MapNote) obj;
         if (note == null) {
-            if (other.note != null)
+            if (other.note != null) {
                 return false;
-        } else if (!note.equals(other.note))
+            }
+        } else if (!note.equals(other.note)) {
             return false;
+        }
         if (user == null) {
-            if (other.user != null)
+            if (other.user != null) {
                 return false;
-        } else if (!user.equals(other.user))
+            }
+        } else if (!user.equals(other.user)) {
             return false;
+        }
         return true;
     }
 
